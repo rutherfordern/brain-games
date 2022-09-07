1b83f5be6f3113a07c0b6d5825424834ba20d27e
Egor <glavnegor@yandex.ru>
2022-09-07 22:32:53 +0300

    update project

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+/* eslint-disable */
+
 import { welcomeQuestion } from '../src/cli.js';
 
 welcomeQuestion();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
+/* eslint-disable */
 import readlineSync from 'readline-sync';
 
 export const welcomeQuestion = () => {
     console.log('Welcome to the Brain Games!');
-    
-    let name = readlineSync.question('May I have your name?\n');
+
+    const name = readlineSync.question('May I have your name?\n');
     console.log("Hello, dear " + name + "!");
 };
