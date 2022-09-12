dd80b3e530494b0629fe3f4fd39640c446bab279
Egor <glavnegor@yandex.ru>
2022-09-12 18:25:24 +0300

    fix brain-prime

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,15 +1,13 @@
-/* eslint-disable */
 import readlineSync from "readline-sync";
 import { generateRandomNumber } from "../modules/generate-random-number.js";
 import { isPrime } from "../modules/check-prime-num.js";
 
-
-function gameLogic(generateNum, userAnswer) { 
-  let result = isPrime(generateNum);
+function gameLogic(generateNum, userAnswer) {
+  const result = isPrime(generateNum);
 
   const correctAnswer = "Correct!";
   const wrongAnswer = "'yes' is wrong answer ;(. Correct answer was 'no'.";
-  
+
   if (result) {
     return userAnswer === "yes" ? correctAnswer : wrongAnswer;
   }
@@ -17,27 +15,27 @@ function gameLogic(generateNum, userAnswer) {
 }
 
 export function primeGame() {
-    console.log("Welcome to the Brain Games!");
-  
-    const name = readlineSync.question("May I have your name? ");
-    console.log(`Hello, dear ${name}!`);
-  
-    console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
-  
-    let counter = 0;
-    let result = "";
-  
-    do {
-      const question = generateRandomNumber(2, 20);
-      console.log(`Question: ${question}`);
-  
-      const userAnswer = readlineSync.question("Your answer: ");
-  
-      result = gameLogic(question, userAnswer);
-      console.log(result);
-  
-      counter += 1;
-    } while (counter < 3 && result === "Correct!");
-  
-    return result === "Correct!" ? console.log(`Congratulations, ${name}!`) : console.log(`Let's try again, ${name}!`);
-  }
+  console.log("Welcome to the Brain Games!");
+
+  const name = readlineSync.question("May I have your name? ");
+  console.log(`Hello, dear ${name}!`);
+
+  console.log("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+  let counter = 0;
+  let result = "";
+
+  do {
+    const question = generateRandomNumber(2, 20);
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question("Your answer: ");
+
+    result = gameLogic(question, userAnswer);
+    console.log(result);
+
+    counter += 1;
+  } while (counter < 3 && result === "Correct!");
+
+  return result === "Correct!" ? console.log(`Congratulations, ${name}!`) : console.log(`Let's try again, ${name}!`);
+}
